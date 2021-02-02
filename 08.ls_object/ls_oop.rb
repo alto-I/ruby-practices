6054d535ee62c5aa64b279560ba6d88b0fa11a01
alto-I <blmaldiy@gmail.com>
2021-02-02 22:02:40 +0900

    my-ls_object

---
+++ 08.ls_object/ls_oop.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+require 'pathname'
+require './lib/ls'
+
+terminal_width = `tput cols`.to_i
+opt = OptionParser.new
+params = { long_format: false, reverse: false, include_dot_file: false}
+opt.on('-a') { |v| params[:include_dot_file] = v }
+opt.on('-l') { |v| params[:long_format] = v }
+opt.on('-r') { |v| params[:reverse] = v }
+opt.parse!(ARGV)
+path = ARGV[0] || '.'
+pathname = Pathname(path)
+
+ls = Ls.new(pathname, terminal_width, **params)
+ls.output
